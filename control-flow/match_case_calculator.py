780c858442d207d013f8c996d4f65e7f0761368f
bikerironald <bikerironald@gmail.com>
2025-05-31 13:33:44 +0300

    Refactor match case calculator by removing commented-out code and improving division handling

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,20 +1,4 @@
-# num1 = input("Enter the first number: ")
-# num2 = input("Enter the second number: ")
-# operation = input("Enter the operation (+, -, *, /): ")
-# def calculate(num1, num2, operation):
-#     match operation:
-#         case '+':
-#            print("num1+num2")
-#         case '-':
-#             print("num1-num2")
-#         case '*':
-#             print("num1*num2")
-#         case '/':
-#             print("num1/num2")
-# result = calculate((num1), (num2), operation)
-# print(f"The result is: {result}")
-
-
+# calculator
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 operation = input("Enter the operation (+, -, *, /): ")
@@ -28,12 +12,6 @@ def calculate(num1, num2, operation):
         case '*':
             print(num1 * num2)
         case '/':
-            if num2 != 0:
-                print(num1 / num2)
-            else:
-                return "Error: Division by zero"
-        case _:
-            return "Invalid operation"
-
+            print(num1 / num2)
 result = calculate(num1, num2, operation)
 print(f"The result is: {operation}")
