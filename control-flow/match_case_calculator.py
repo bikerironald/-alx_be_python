54ca50bf20678c9d5ae056f5ae88535437cacf18
bikerironald <bikerironald@gmail.com>
2025-05-31 13:56:21 +0300

    Refactor match case calculator to improve structure and remove unnecessary print statements

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,40 +1,21 @@
-# # Simple Calculator with Match Case
-# num1 = int(input("Enter the first number: "))
-# num2 = int(input("Enter the second number: "))
-# operation = input("Enter the operation (+, -, *, /): ")
-# def calculate(num1, num2, operation):
-#     match operation:
-#         case '+':
-#             print(num1+num2)
-#         case '-':
-#             print (num1-num2)
-#         case '*':
-#             print(num1*num2)
-#         case '/':
-#             print(num1 / num2)
-# result = calculate(num1, num2, operation)
-# print(f"The result of {result}")
-
-# Simple Calculator with Match Case
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-operation = input("Enter the operation (+, -, *, /): ")
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+operation = input ("Choose the operation (+, -, *, /): ")
 
 def calculate(num1, num2, operation):
     match operation:
-        case '+':
-            return num1 + num2
-        case '-':
-            return num1 - num2
-        case '*':
-            return num1 * num2
-        case '/':
-            if num2 != 0:
-                return num1 / num2
-            else:
-                return "Error: Division by zero."
-        case _:
-            return "Invalid operation."
+        case "+":
+            print(num1+num2)
+        case "-":
+            print(num1-num2)
+        case "*":
+            print(num1*num2)
+        case "/":
+            print(num1/num2)
 
 result = calculate(num1, num2, operation)
-print(f"The result is: {result}")
+print(f'The result is [result]')
+        
+# redone with match case
+
+
