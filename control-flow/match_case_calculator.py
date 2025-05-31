27a600b05c50fbae0d844ff1e6bbec13ddcf86f7
bikerironald <bikerironald@gmail.com>
2025-05-31 13:30:50 +0300

    Fix calculation output and change input type to int in match case calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -15,25 +15,25 @@
 # print(f"The result is: {result}")
 
 
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 operation = input("Enter the operation (+, -, *, /): ")
 
 def calculate(num1, num2, operation):
     match operation:
         case '+':
-            return num1 + num2
+            print(num1 + num2)
         case '-':
-            return num1 - num2
+            print(num1 - num2)
         case '*':
-            return num1 * num2
+            print(num1 * num2)
         case '/':
             if num2 != 0:
-                return num1 / num2
+                print(num1 / num2)
             else:
                 return "Error: Division by zero"
         case _:
             return "Invalid operation"
 
 result = calculate(num1, num2, operation)
-print(f"The result is: {result}")
+print(f"The result is: {operation}")
