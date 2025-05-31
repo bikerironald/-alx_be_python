15a240a94e85c711f3a18dc485043575e9267ba2
bikerironald <bikerironald@gmail.com>
2025-05-31 12:13:30 +0300

    Refactor variable name for weather input in weather recommendation system

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,11 @@
 # Weather Recommendation System
 # This task aims to demonstrate the use of if, elif, and else statements to make decisions in a program.
-user = input(" What's the weather like today? (sunny/rainy/cold): ")
-if user.lower() == "sunny":
+Weather = input(" What's the weather like today? (sunny/rainy/cold): ").lower()
+if Weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif user.lower() == "rainy":
+elif Weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif user.lower() == "cold":
+elif Weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")  
