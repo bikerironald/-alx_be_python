cffe3b24b1212db7f291aa182a86b242214d8000
bikerironald <bikerironald@gmail.com>
2025-05-31 17:31:17 +0300

    Refactor multiplication table output to store product in a variable

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,4 +3,5 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1, 11):
-    print(f"{number} x {i} = {number * i}")
+    product = number * i
+    print(f"{number} x {i} = {product}")
